0bb591866a32b3daed0e5bef81fa86c804673077
Barthelemy Drabczuk <37699208+Barthelemy-Drabczuk@users.noreply.github.com>
2023-02-13 20:21:51 +0100

    Db management init (#1)
    * adding last CRUD function for the db before writing tests
    
    * implementing the first features to pass the insert test

--- src/clip_user.py
+++ src/clip_user.py
@@ -2,9 +2,24 @@ class ClipUser:
     """_summary_
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, username: str, password: str, email: str) -> None:
+        self.username = username
+        self.password = password
+        self.email = email
+        self.hash = hash(self.username \
+            + self.password \
+            + self.email
+            )
+
+
+    def to_json(self) -> dict:
+        return {
+            "id": self.hash,
+            "username": self.username,
+            "password": self.password,
+            "email": self.email,
+        }
 
 
     def __str__(self) -> str:
-        pass
+        return str(self.to_json())
