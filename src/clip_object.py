b097fc56d0da419dcc52515a3592161f5f73d33a
barthelemy-drabczuk <percevase@gmail.com>
2023-02-17 19:22:28 +0100

    first test and writing correspond methods

--- src/clip_object.py
+++ src/clip_object.py
@@ -1,3 +1,11 @@
 class ClipObject:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, content: str) -> None:
+        self.content = content
+
+    
+    def to_json(self) -> dict:
+        return {"content": self.content}
+    
+
+    def get_content(self) -> str:
+        return self.content
--- src/tests/clip_object_test.py
+++ src/tests/clip_object_test.py
@@ -5,3 +5,15 @@ from clip_object import ClipObject
 
 def test_clip_object_init():
     obj = ClipObject("azerty")
+
+    assert obj.get_content() is not None
+    assert isinstance(obj.get_content(), str)
+    assert obj.get_content() == "azerty"
+
+
+def test_clip_object_json():
+    obj = ClipObject("azerty")
+
+    assert obj.to_json() is not None
+    assert isinstance(obj.to_json(), dict)
+    assert obj.to_json() == {"content": obj.get_content()}
